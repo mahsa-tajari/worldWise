e75588111b5215960fdab5aa8ac62e729f2c91f9
mahsa-tajari <khodemahsatjr@gmail.com>
2023-12-30 10:28:36 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import "./App.css";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path="product" element={<Product />}></Route>
           <Route path="pricing" element={<Pricing />}></Route>
           <Route path="app" element={<AppLayout />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </>
