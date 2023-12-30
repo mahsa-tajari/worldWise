bc7656225440bd8ad1a9af2aeec4e26a3c7af1da
mahsa-tajari <khodemahsatjr@gmail.com>
2023-12-30 19:15:42 +0330

    Update Map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import './'
+import React from "react";
+import styles from "./Map.module.css";
 export default function Map() {
-  return (
-    <div></div>
-  )
+  return <div className={styles.mapContainer}>MAp</div>;
 }
