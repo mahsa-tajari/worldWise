488181e548d7400ec40d2c7a563c1d8cde7c1517
mahsa-tajari <khodemahsatjr@gmail.com>
2023-12-30 10:27:38 +0330

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import styles from "./Login.module.css";
-
+import { useState } from "react";
+import AppNav from "../components/AppNav";
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
@@ -7,6 +8,7 @@ export default function Login() {
 
   return (
     <main className={styles.login}>
+      <AppNav />
       <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
