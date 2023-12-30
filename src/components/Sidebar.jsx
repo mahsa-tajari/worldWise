fe64bbb35cbdfa75d9ad11e2bcbcb91e13a74913
mahsa-tajari <khodemahsatjr@gmail.com>
2023-12-30 10:27:55 +0330

    Create Sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./Sidebar.module.css";
+import Logo from './Logo';
+import App
+export default function Sidebar() {
+  return <div className={styles.Sidebar}>
+
+  </div>;
+}
