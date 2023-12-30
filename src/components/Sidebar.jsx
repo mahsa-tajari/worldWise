df5508460d2564014e15e8ca235f3c870621ec78
mahsa-tajari <khodemahsatjr@gmail.com>
2023-12-30 19:15:29 +0330

    side bar structure

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import styles from "./Sidebar.module.css";
-import Logo from './Logo';
-import App
+import Logo from "./Logo";
+import { Outlet } from "react-router-dom";
+import AppNav from "./AppNav";
 export default function Sidebar() {
-  return <div className={styles.Sidebar}>
-
-  </div>;
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <Outlet />
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WolrldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
 }
