c7f7c80fe6b0f6da45eb4a841b90b68ff3a1ccc0
mahsa-tajari <khodemahsatjr@gmail.com>
2024-01-05 23:55:04 +0330

    Update AppLayout.jsx

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./AppLayout.module.css";
 import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
+import User from "../components/User";
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
